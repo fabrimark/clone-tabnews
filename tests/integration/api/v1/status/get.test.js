2f25aaf4b3475ec8a64c05f71c591559f6dbaf61
Fabricio Marques <127909251+fabrimark@users.noreply.github.com>
2026-04-29 00:48:43 +0000

    add `database.js` and `.env` files

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,3 @@
-import { expectNoPendingImmediates } from "next/dist/server/node-environment-extensions/fast-set-immediate.external";
-
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
